e21ac0a09ed0e7f8117f03be7b692f71a992f856
Fabian Pichler <fabian.pichler@synventis.com>
2024-10-13 20:26:50 +0200

    improve routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,22 @@
+import LayoutView from '@/views/BellisTest.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import LayoutView from '../views/BellisTest.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Document_Default',
-      redirect: '/Content/WebHome',
-    },
-    {
-      path: '/:val',
-      name: 'Document_Partial',
-      redirect: to => {
-        console.log(to.path);
-        if (to.path.endsWith('/')) {
-          return { path: to.path + 'WebHome' }
-        }
-        return { path: '/Content' + to.path }
-      },
-    },
-    {
-      path: '/:spacename/:docname',
+      path: '/:part1?/:part2?',
       name: 'Document',
-      props: true,
+      props: route => {
+        console.log('Document route', route.path.endsWith('/'), 'part1', route.params.part1, 'part2', route.params.part2);
+        const spacename = (((route.path.endsWith('/') && !!route.params.part1) || (!!route.params.part2 && !!route.params.part1)) ? route.params.part1 : undefined) ?? 'Content';
+        const docname = [route.params.part2, !route.path.endsWith('/') ? route.params.part1 : undefined, 'WebHome'].find(part => !!part && part.length > 0);
+        console.log('Document params space', spacename, 'document', docname);
+        return {
+          spacename: spacename,
+          docname : docname,
+        };
+      },
       component: LayoutView,
     },
   ],
