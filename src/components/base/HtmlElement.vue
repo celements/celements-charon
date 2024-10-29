d6842af78bc75365721d577e966f6e3daa0411ca
Fabian Pichler <fabian.pichler@synventis.com>
2024-10-29 23:45:25 +0100

    remove unused project-template vue-compontents, change to useTemplateRef

--- src/components/base/HtmlElement.vue
+++ src/components/base/HtmlElement.vue
@@ -3,14 +3,18 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, onMounted, ref, watch } from 'vue';
-const htmlElem = ref();
+import { defineProps, onMounted, useTemplateRef, watch } from 'vue';
+const htmlElem = useTemplateRef('htmlElem');
 const props = defineProps<{
   html: string;
 }>();
 const updateContent = (html: string) => {
-  htmlElem.value.replaceChildren();
-  htmlElem.value.insertAdjacentHTML('afterbegin', html);
+  if (htmlElem.value) {
+    htmlElem.value.replaceChildren();
+    htmlElem.value.insertAdjacentHTML('afterbegin', html);
+  } else {
+    console.error('template reference "htmlElem" not found');
+  }
 };
 watch(props, (props) => updateContent(props.html));
 onMounted(() => updateContent(props.html));
